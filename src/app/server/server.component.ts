c5d2a77be8771556d3b241641877aeb65bca9782
Jonathan <jonathan23986@gmail.com>
2021-08-25 07:33:35 +0300

    structural directives

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -9,6 +9,18 @@ export class ServerComponent {
   serverId = 10;
   serverStatus = 'offline';
 
+  constructor() {
+    this.serverStatus = this.randomStatus();
+  }
+
+  private randomStatus() {
+    return Math.random() > 0.5 ? 'online' : 'offline';
+  }
+
+  getStatusColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
+  }
+
   getServerStatus() {
     return this.serverStatus;
   }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,6 +9,7 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
   serverName = '';
+  serverCreated = false;
   servers = [];
 
   constructor() {
@@ -18,6 +19,7 @@ export class ServersComponent implements OnInit {
   ngOnInit(): void {}
 
   onCreateServer() {
+    this.serverCreated = true;
     this.serverCreationStatus = `Server was created! Name is ${this.serverName}`;
   }
 
